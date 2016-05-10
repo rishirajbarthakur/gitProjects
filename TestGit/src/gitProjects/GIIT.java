24152dd659114d4cc4338f321f1fe082fd09e90a
rishirajbarthakur <rishiraj.barthakur@siqes.com>
2016-05-10 18:03:44 +0530

    Changing Gitt.

--- TestGit/src/gitProjects/GIIT.java
+++ TestGit/src/gitProjects/GIIT.java
@@ -4,7 +4,8 @@ public class GIIT {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Testing");
+		System.out.println("Testing");
+		System.out.println("Hello Git");
 	}
 
 }
